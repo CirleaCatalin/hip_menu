8c7b17a36407ba6bb5a92de271a36172c3fbe4aa
CirleaCatalin <cirleacatalin98@gmail.com>
2025-11-21 20:46:21 +0200

    folder structure and data models

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hip_menu/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(this.category, {super.key});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
